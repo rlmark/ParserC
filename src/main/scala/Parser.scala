33b5af0c2f303f59eb4d8ebec5fa90224de3767e
Kate Fulton <katefulton@richard.lan>
2020-07-12 22:51:19 -0700

    Added tests for additional primitive parsers

--- src/main/scala/Parser.scala
+++ src/main/scala/Parser.scala
@@ -2,4 +2,8 @@ object Parser {
   type Parser[A] = String => List[(A, String)]
 
   def const[A](a: A): Parser[A] = input => List((a, input))
+
+  def zero[A](a: A): Parser[A] = ???
+
+  def item[A]: Parser[A] = ???
 }
--- src/test/scala/ParserSpec.scala
+++ src/test/scala/ParserSpec.scala
@@ -2,10 +2,27 @@ import org.scalatest.flatspec.AnyFlatSpecLike
 import org.scalatest.matchers.should.Matchers
 
 class ParserSpec extends AnyFlatSpecLike with Matchers {
-  "Parser" should "have a const function" in {
-    val int = 1
+  "const" should "succeed returning value without consuming any input" in {
+    val value = 1
     val input = "1"
+    Parser.const(value)(input) shouldBe List((value, input))
+  }
 
-    Parser.const(int)(input) shouldBe List((int, input))
+  it should "should handle empty input" in {
+    val value = 1
+    Parser.const(value)("") shouldBe List((value, ""))
+  }
+
+  "zero" should "fail regardless of input" in {
+    Parser.zero("fail")("1") shouldBe List()
+  }
+
+  "item" should "consume the first character if input is non-empty" in {
+    val input = "consume"
+    Parser.item(input) shouldBe List(input.head, input.tail)
+  }
+
+  it should "fail if input is empty" in {
+    Parser.item("") shouldBe List()
   }
 }
